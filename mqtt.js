1b777acd9b445f3136767a95e092ff254ff70fce
Robbetest123 <robbe.croes@student.vives.be>
2020-05-23 15:19:50 +0200

    commit_6

--- mqtt.js
+++ mqtt.js
@@ -7,7 +7,7 @@ var client  = mqtt.connect('mqtt://test.mosquitto.org')
 client.on('connect', function () {
   client.subscribe('presence', function (err) {
     if (!err) {
-      client.publish('presence', 'Hello has subscribed')
+      client.publish('presence', 'Hello has robbe subscribed')
     }
     data = GPIO.readInputs();
   })

b3ea33eabdc2601efc1c52d4bb1fe1f9e6e240bb
Robbetest123 <robbe.croes@student.vives.be>
2020-05-23 15:12:17 +0200

    commit_5

--- index.js
+++ index.js
@@ -7,25 +7,6 @@ const mqtt = require('./mqtt').default
 
 var data;
 
-
-// app.get('/', function(req, res){
-//   res.sendfile(__dirname+'/index.html');
-// });
-
-// app.get('/buttons.html', function(req, res){
-//   res.sendfile(__dirname+'/buttons.html');
-// });
-
-// app.get('/leds.html', function(req, res){
-//   res.sendfile(__dirname+'/leds.html');
-// });
-
-// app.get('/temperature.html', function(req, res){
-//   res.sendfile(__dirname+'/temperature.html');
-// });
-
-
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
